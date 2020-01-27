4e1257d0f7655f39ebfc6e9f0c30079ae70a395b
yvanvds <yvan@attr-x.net>
2020-01-27 21:07:45 +0100

    without factory

---
+++ GreatBallsOfFire/Fireball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GreatBallsOfFire
+{
+    class Fireball
+    {
+        string color;
+        int size;
+        float damage;
+        float healing;
+        float dot;
+
+        public Fireball(string color, int size, float damage, float healing, float dot)
+        {
+            this.color = color;
+            this.size = size;
+            this.damage = damage;
+            this.healing = healing;
+            this.dot = dot;
+        }
+
+        public void Cast()
+        {
+            Console.WriteLine(this);
+        }
+
+        public override string ToString()
+        {
+            string result = "Casting ";
+            if (size > 10)
+            {
+                result += "big ";
+            } else if (size > 5)
+            {
+                result += "medium ";
+            } else {
+                result += "small ";
+            }
+            result += color + " fireball with ";
+            if (damage > 0) result += damage + " damage ";
+            if (healing > 0) result += healing + " healing ";
+            if (dot > 0) result += dot + " damage over time ";
+            return result;
+        }
+    }
+}
---
+++ GreatBallsOfFire/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace GreatBallsOfFire
+{
+    class Program
+    {
+        static bool hero = false;
+
+        static void Main(string[] args)
+        {
+            SMUtils.Menu menu = new SMUtils.Menu();
+            menu.AddOption('1', "Enter Hero Mode", () => hero = true);
+            menu.AddOption('2', "Exit Hero Mode", () => hero = false);
+            menu.AddOption('3', "Cast Healing Ball", castHealing);
+            menu.AddOption('4', "Cast Damage Ball", castDamage);
+            menu.AddOption('5', "Cast Shadow Ball", castShadow);
+
+            menu.Start();
+        }
+
+        private static void castShadow()
+        {
+            Fireball ball;
+            if (hero)
+            {
+                ball = new Fireball("purple", 6, 0, 0, 7);
+            } else
+            {
+                ball = new Fireball("deep purple", 3, 0, 0, 3.5f);
+            }
+            ball.Cast();
+        }
+
+        private static void castDamage()
+        {
+            Fireball ball;
+            if (hero)
+            {
+                ball = new Fireball("red", 12, 8, 0, 0);
+            }
+            else
+            {
+                ball = new Fireball("pink", 6, 5.6f, 0, 0);
+            }
+            ball.Cast();
+        }
+
+        private static void castHealing()
+        {
+            Fireball ball;
+            if (hero)
+            {
+                ball = new Fireball("green", 20, 0, 12, 0);
+            }
+            else
+            {
+                ball = new Fireball("slightly green", 2, 0, 3, 0);
+            }
+            ball.Cast();
+        }
+    }
+}
